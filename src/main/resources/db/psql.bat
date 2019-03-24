cfa5c532a502809483997bbbedbaf41ddced478c
mslmnb <mslmn@rambler.ru>
2019-03-24 10:49:02 +0600

    Project instruction has been added

--- src/main/resources/db/psql.bat
+++ src/main/resources/db/psql.bat
@@ -1,4 +1,8 @@
-cd C:\Program Files\PostgreSQL\10\bin
-psql.exe -h localhost -U postgres -d postgres -p 5432 -a -f C:\java_project\hospital-master\src\main\resources\db\initdb.sql
+cd C:\Program Files (x86)\PostgreSQL\10\bin
+
 SET PGCLIENTENCODING=utf-8
-psql.exe -h localhost -U user -d hospital -p 5432 -a -f C:\java_project\hospital-master\src\main\resources\db\\populateDB.sql
+
+
+psql.exe -h localhost -U postgres -d postgres -p 5432 -a -f C:\java_project\hospital-master\src\main\resources\db\initdb.sql
+
+psql.exe -h localhost -U user -d hospital -p 5432 -a -f C:\java_project\hospital-master\src\main\resources\db\populateDB.sql
